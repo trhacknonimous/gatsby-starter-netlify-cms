43193bcca9712516492299a7ce321e83929023c9
trhacknonimous <82779890+trhacknonimous@users.noreply.github.com>
2021-06-28 09:59:55 +0200

    Add files via upload

---
+++ src/pages/about/test.php
@@ -0,0 +1,29 @@
+<html><head><link rel="icon" type="image/png" href="https://h.top4top.io/p_198923pip1.png"/><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><meta property="og:image" content="https://h.top4top.io/p_198923pip1.png"><meta property="og:title" content="#LulzSecPh"><meta property="og:description" content="#OpIsrael"/><title>Defaced By #LulzSecPh</title><BODY ONCONTEXTMENU='return false;' ONKEYDOWN='return false;' ONMOUSEDOWN='return false;'>
+<link href="https://fonts.googleapis.com/css2?family=Graduate&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Reggae+One&display=swap" rel="stylesheet">
+<style type="text/css"> .lagu{ background:transparent; border:2px solid red; font-family:Share Tech Mono; color:white; font-size:15x; font-weight:bold; padding:3px 29px; text-decoration:none; text-shadow:0px 0px 0px ; } </style>
+
+<script> function play(){ var audio = document.getElementById('lagu'); audio.play(); } function liat(){ document.getElementById('galiat').style.visibility='visible'; } </script>
+</head><body BGCOLOR="black"><style type="text/css"> 	body{ background-color: black;background-size : cover;background-position: center;background-image: url("");} div.content{ color: white; font-family: 'Oswald', sans-serif; font-size: 40px; text-align: center; } div.body{ color: white; font-family: 'Oswald', sans-serif; font-size: 15px; text-align: center; } div.xxx{ color: white; font-family: tahoma; font-size: 17px; text-align: center; } div.xxxx{ color: white; font-family:courier; font-size: 17px; text-align: center; } </style>
+<style>.blink {
+animation: blink-animation 1s steps(5, start) infinite;
+-webkit-animation: blink-animation 1s steps(5, start) infinite;
+}
+@keyframes blink-animation {
+to {
+visibility: hidden;
+}
+}
+@-webkit-keyframes blink-animation {
+to {
+visibility: hidden;
+}
+}
+</style><script src="https://cdn.rawgit.com/bungfrangki/efeksalju/2a7805c7/efek-salju.js" type="text/javascript"></script><table width="100%" height="100%"><td align="center"><center><img alt="M4L FamilY" height="226px; " id="Header1_headerimg" src="https://i.gifer.com/WczY.gif" style="display: block" width="244px; "><b></font><font class="blink" face="Reggae One" size="6" color="white">[ Trhacknonymous Area Occupied ]</font>  </b><br><br></font><font face="Times" color="white" size="5"><b><br>
+    <font size="4"><a style="color:red">trhacknon</a><br>Capitalism Destroy The Planet,<BR> 
+        watching porns, or just waiting for your salary,
+          why don't you concentrate on your website? <br>your not securing your website.<BR> 
+          You are not protecting your customers or clients especially
+                      their information.<br>
+<font Color=Red>#<font Color=White>OpWakeUp <font Color=Red>#<font Color=White>UpDateSysteam <font Color=Red>#<font Color=White>D4RK-D3VIL<font Color=Red>#<font Color=White>HAC4ALL SECURITY TEAM/REDBLOOD
+<br><br><font face="times" color="red" size="4"><br><b>&#8226<font color="white"> D4RK_SL33P <font color="red">&#8226<font color="red"> ktm <font color="red">&#8226<font color="white"> Bbloc <font color="red">&#8226<font color="white"> D4RK-D3VIL <font color="red">&#8226<font color="white"> destroy capitamism <font color="red">&#8226<font color="white"> trhacknonymous <font color="red">&#8226<font color="white"> emmytromal <font color="red">&#8226<font color="white"> PSWI-MHIE <font color="red">&#8226</b><p><font color="white" face="times"><br><b>&copy; 2021 HAC4ALL SECURITY TEAM DEFACER</font></b></marquee><br><br><button class="lagu" onclick="play();liat();"><font face="graduate" size="4" color="" >Play Music</font></button><audio id="lagu" src="https://i.top4top.io/m_1998yrne61.mp3"></audio><font color="white"></font></button></center></html>
